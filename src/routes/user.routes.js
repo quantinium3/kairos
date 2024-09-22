7ad5e3ff1157ec863b633369e9088854bb761e70
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-23 04:21:46 +0530

    added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controllers.js';
+import {
+    changePassword,
+  deleteUser,
+  getUserProfile,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from '../controllers/user.controllers.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/profile').get(verifyJWT, getUserProfile);
+router.route('/profile').delete(verifyJWT, deleteUser);
+router.route('/change-password').post(verifyJWT, changePassword)
 
 export default router;
