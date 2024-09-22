1d4e9b65d0f0eda3a02e1c818003a9f6248c5b73
QuantiniumX <vaibhavvx331@gmail.com>
2024-09-23 04:20:16 +0530

    smth

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
-import crypto from "crypto"
 export const DB_NAME = "lomes.db"
 
-export const ACCESS_TOKEN_SECRET = crypto.getRandomValues(64).toString('hex');
-export const REFRESH_TOKEN_SECRET = crypto.getRandomValues(64).toString('hex');
+export const ACCESS_TOKEN_SECRET = "1234567890qwertyuiopasdfghjklzxcvbnmi";
+export const REFRESH_TOKEN_SECRET = "zxcvbnmasdfghjklqwertyuiop1234567890"
 export const ACCESS_TOKEN_EXPIRY = '15m'
 export const REFRESH_TOKEN_EXPIRY = '30d';
